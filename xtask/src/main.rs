7d1878a6dc6d49fbfb2d0a8cd6d72c6911a5fa74
Nicholas Bishop <nbishop@nbishop.net>
2022-03-20 23:31:02 -0400

    Clean up disas code

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -70,7 +70,7 @@ fn main() -> Result<()> {
         let offset = text_section.virtual_address as usize;
         let start = fi.addr_range.start - offset;
         let end = fi.addr_range.end - offset;
-        disas(&text_data[start..end], 0);
+        disas(&text_data[start..end], fi.addr_range.start as u64);
     }
 
     Command::with_args(
@@ -144,51 +144,22 @@ fn load_pdb(path: &str) -> Result<Vec<FuncInfo>> {
     Ok(output)
 }
 
+// Based on example in https://docs.rs/iced-x86/latest/iced_x86/index.html
 fn disas(bytes: &[u8], start_addr: u64) {
     let mut decoder = Decoder::with_ip(64, bytes, start_addr, DecoderOptions::NONE);
 
-    // Formatters: Masm*, Nasm*, Gas* (AT&T) and Intel* (XED).
-    // For fastest code, see `SpecializedFormatter` which is ~3.3x faster. Use it if formatting
-    // speed is more important than being able to re-assemble formatted instructions.
     let mut formatter = NasmFormatter::new();
-
-    // Change some options, there are many more
     formatter.options_mut().set_digit_separator("`");
     formatter.options_mut().set_first_operand_char_index(10);
-
-    // String implements FormatterOutput
     let mut output = String::new();
-
-    // Initialize this outside the loop because decode_out() writes to every field
     let mut instruction = Instruction::default();
 
-    // The decoder also implements Iterator/IntoIterator so you could use a for loop:
-    //      for instruction in &mut decoder { /* ... */ }
-    // or collect():
-    //      let instructions: Vec<_> = decoder.into_iter().collect();
-    // but can_decode()/decode_out() is a little faster:
     while decoder.can_decode() {
-        // There's also a decode() method that returns an instruction but that also
-        // means it copies an instruction (40 bytes):
-        //     instruction = decoder.decode();
         decoder.decode_out(&mut instruction);
 
-        // Format the instruction ("disassemble" it)
         output.clear();
         formatter.format(&instruction, &mut output);
 
-        // Eg. "00007FFAC46ACDB2 488DAC2400FFFFFF     lea       rbp,[rsp-100h]"
-        print!("{:016X} ", instruction.ip());
-        let start_index = (instruction.ip() - start_addr) as usize;
-        let instr_bytes = &bytes[start_index..start_index + instruction.len()];
-        for b in instr_bytes.iter() {
-            print!("{:02X}", b);
-        }
-        if instr_bytes.len() < 10 {
-            for _ in 0..10 - instr_bytes.len() {
-                print!("  ");
-            }
-        }
-        println!(" {}", output);
+        println!("{:08x} {}", instruction.ip(), output);
     }
 }
